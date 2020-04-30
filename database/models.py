2cb3536df21519bbbc5f466c2d19d61837463ca5
Mauricio Degregori <degregori2@gmail.com>
2020-04-30 16:20:31 -0500

    added a call to password

--- database/models.py
+++ database/models.py
@@ -4,7 +4,7 @@ from database import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
-    password = db.Column(db.String, nullable=False)
+    password = db.Column(db.String(), nullable=False)
     # posts = db.relationship('Post', backref="author", lazy=True)
     
     # def __repr__(self):
