4027f5b75d3a7e038aa17d06f8b2c41fdb49c10d
Mauricio Degregori <degregori2@gmail.com>
2020-05-01 01:05:15 -0500

    added new tables

--- database/models.py
+++ database/models.py
@@ -10,3 +10,42 @@ class User(db.Model):
     # def __repr__(self):
     #     return f"User('{self.id}', '{self.username}', '{self.password}')"
 
+class Room(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    description = db.Column(db.String(1024), nullable=False)
+    x = db.Column(db.Integer, nullable=True)
+    y = db.Column(db.Integer, nullable=True)  
+    item = db.relationship('Item', backref = "room", lazy=True)
+
+    def __init__(self, name, description, x, y, item=None):
+        self.name = name
+        self.description = description
+        self.x = x
+        self.y = y
+        self.item = item if item is not None else []
+
+    def __str__(self):
+        return f"{self.name}, {self.description}, {self.x}, {self.y}, {self.item}"
+    
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    player_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
+    room_id   = db.Column(db.Integer, db.ForeignKey('room.id'), nullable=True)
+
+
+    def __init__(self, name, description, player_id=None, room_id=None):
+        self.name = name
+        self.description = description
+        self.player_id = player_id
+        self.room_id = room_id
+
+    def __str__(self):
+        return f"{self.name}, {self.description}, {player_id}, {room_id}"
+    
+      
+
+
+
